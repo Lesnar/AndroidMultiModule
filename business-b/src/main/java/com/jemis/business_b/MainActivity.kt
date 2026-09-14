b7b41196f5bdb12f284905e248ae403abc087cb6
WangJunming <extern.junming.wang@cariad-technology.cn>
2026-09-13 23:57:06 -0700

    Compose 调用Android View体系

--- business-b/src/main/java/com/jemis/business_b/MainActivity.kt
+++ business-b/src/main/java/com/jemis/business_b/MainActivity.kt
@@ -1,16 +1,21 @@
 package com.jemis.business_b
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import com.jemis.base.router.RouterPath
 import com.therouter.TheRouter
 import com.therouter.router.Autowired
@@ -50,10 +55,22 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun BusinessBScreen(modifier: Modifier = Modifier, from: String = "") {
     Scaffold(modifier = modifier.fillMaxSize()) { innerPadding ->
-        Text(
-            text = "Hello business-b\n来自：$from",
-            modifier = Modifier.padding(innerPadding),
-        )
+        Column(modifier = Modifier.padding(innerPadding)) {
+            Text(
+                text = "Hello business-b\n来自：$from",
+            )
+            HorizontalDivider()
+            AndroidView(
+                factory = {
+                    TextView(it)
+                },
+                update = {
+                    it.text = "haha"
+                }
+            )
+            HorizontalDivider()
+
+        }
     }
 }
 
